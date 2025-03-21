afe7884be2c51b36afabb9b07f70371ffe5ad3ec
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2025-03-21 19:00:06 +0530

    21/03

---
+++ 13_circle.py
@@ -0,0 +1,5 @@
+#wap to find area of circle
+print("this is cricle area find app")
+radius=eval(input("enter a radius :"))
+area=3.141*radius*radius
+print(f"area of circle = {area}")
---
+++ 14_tringle.py
@@ -0,0 +1,6 @@
+#wap to find area of tringle
+print("tringle area find app....")
+base=eval(input("enter base : "))
+height=eval(input("enter height : "))
+area=1/2*base*height
+print("area of tringle : ",area)
