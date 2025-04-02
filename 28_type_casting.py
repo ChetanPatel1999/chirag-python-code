fbcb14189f8f5b7e705fa4267e9161708a877ee8
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2025-04-02 18:08:22 +0530

    2/4

---
+++ 28_type_casting.py
@@ -0,0 +1,22 @@
+#type conversion or type casting
+# automatically 
+# manually 
+a=12
+print(a)
+b=str(a)
+print(b)
+print(type(a))
+print(type(b))
+print(str(10))
+print(str(10.1))
+print(str("hi"))
+print(int(10))
+print(int("10"))
+print(int(12.56))
+#print(int("10.1")) # show error
+#print(int("hi")) # show error
+print(float(10))
+print(float(10.89))
+print(float("10"))
+print(float("10.90"))
+#print(float("home")) #show error
---
+++ 29_compound_operator.py
@@ -0,0 +1,14 @@
+# example of compound assignment
+a=12
+b=8
+print(f"a = {a}")
+print(f"b = {b}")
+#a+=b
+#a-=b
+#a*=b
+#b*=b
+#a+=5
+#a**=2
+a%=b
+print(f"a = {a}")#4
+print(f"b = {b}")#8
